289181bf396a5bc6d9a896ee6ee1503e6e98fbd1
yaaan7 <163130968+yaaan7@users.noreply.github.com>
2024-04-10 02:45:21 +0900

    Add files via upload

---
+++ 4주차 java class/baseballplayer.js
@@ -0,0 +1,21 @@
+public class baseballplayer{
+    String name;
+    int age;
+    int height;
+    String pitchingkind;
+    int speed;
+}
+
+public class Main {
+    public static void main(String[] args) {
+        baseballplayer gwak = new baseballplayer();
+
+        player1.name = "곽빈";
+        player1.age = 26;
+        player1.height = 187;
+        player1.pitchingkind = "커브";
+        player1.speed = 155;
+}
+
+
+}
